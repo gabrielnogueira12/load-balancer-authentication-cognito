b01a95b9bfb1f7a64382597abb0075c9abcc3b95
Gabriel <gabriel.nogueira142@gmail.com>
2021-08-23 16:19:25 -0300

    Adjust in code

--- loadbalancer/listeners.tf
+++ loadbalancer/listeners.tf
@@ -1,32 +1,37 @@
 resource "aws_lb_listener" "listener80" {
-    load_balancer_arn = aws_lb.myLB.arn
-    port              = "80"
-    protocol          = "HTTP"
+  load_balancer_arn = aws_lb.myLB.arn
+  port              = "80"
+  protocol          = "HTTP"
     
-    default_action {
-        type             = "forward"
-        target_group_arn = aws_lb_target_group.tg01.arn
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
+  }
 }
 
 resource "aws_lb_listener" "listener443" {
-    load_balancer_arn = aws_lb.myLB.arn
-    port              = "443"
-    protocol          = "HTTPS"
-    ssl_policy        = "ELBSecurityPolicy-2016-08"
-    certificate_arn   = var.arn_certificate
-    default_action {
-        type = "authenticate-cognito"
+  load_balancer_arn = aws_lb.myLB.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.arn_certificate
+  default_action {
+    type = "authenticate-cognito"
 
-        authenticate_cognito {
-            user_pool_arn       = var.user_pool_arn
-            user_pool_client_id = var.user_pool_client_id
-            user_pool_domain    = var.user_pool_domain
-        }
+    authenticate_cognito {
+      user_pool_arn       = var.user_pool_arn
+      user_pool_client_id = var.user_pool_client_id
+      user_pool_domain    = var.user_pool_domain
     }
+  }
 
-    default_action {
-        type             = "forward"
-        target_group_arn = aws_lb_target_group.tg01.arn
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg01.arn
+  }
 }
